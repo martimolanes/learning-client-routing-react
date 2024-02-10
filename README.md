96b970ed80bb4ddb0af831a9b36f6105b1cec830
martinho <martinho.n03@gmail.com>
2024-02-10 15:15:43 +0200

    refactor(frontend): API endpoint is a variable

--- README.md
+++ README.md
@@ -9,9 +9,12 @@ vim backend/.env
 ```
 You need to specify the following environment variables in the .env file:
 ```bash
+# default port of the api is
 PORT=6969
+# default docker database url
 DATABASE_URL=mongodb://root:example@localhost:27017
 ```
+> If you want to use a different port, you need to change the frontend port also. `vim frontend/src/contacts.js`
 
 ## setup the database
 ```bash
--- frontend/src/contacts.js
+++ frontend/src/contacts.js
@@ -2,8 +2,10 @@ import axios from "axios";
 import { matchSorter } from "match-sorter";
 import sortBy from "sort-by";
 
+const API_URL = "http://localhost:6969"
+
 export async function getContacts(query) {
-  let response = await axios.get("http://localhost:6969/contacts");
+  let response = await axios.get(API_URL + "/contacts");
   let contacts = response.data;
   if (!contacts) contacts = [];
   if (query) {
@@ -13,24 +15,24 @@ export async function getContacts(query) {
 }
 
 export async function createContact() {
-  let response = await axios.post("http://localhost:6969/contacts");
+  let response = await axios.post(API_URL + "/contacts");
   return response.data;
 }
 
 export async function getContact(id) {
-  let response = await axios.get(`http://localhost:6969/contacts/${id}`);
+  let response = await axios.get(API_URL + `/contacts/${id}`);
   return response.data;
 }
 
 export async function updateContact(id, updates) {
   let response = await axios.patch(
-    `http://localhost:6969/contacts/${id}`,
+    API_URL + `/contacts/${id}`,
     updates,
   );
   return response.data;
 }
 
 export async function deleteContact(id) {
-  let response = await axios.delete(`http://localhost:6969/contacts/${id}`);
+  let response = await axios.delete(API_URL + `/contacts/${id}`);
   return response.data;
 }
